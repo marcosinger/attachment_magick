f41a48b37af84f88d4f9f988c766649858436627
Marco Singer <markaum@gmail.com>
2011-03-18 15:35:23 -0300

    bugfix - case string

--- lib/attachment_magick/dsl.rb
+++ lib/attachment_magick/dsl.rb
@@ -2,7 +2,7 @@ module AttachmentMagick
   class DSL
     attr_reader :styles
     attr_reader :default_options
-    
+
     def initialize(set, default_grids)
       @set    = set
       @styles = {}
@@ -12,22 +12,26 @@ module AttachmentMagick
 
     def method_missing(name, *params, &blk)
       options = params.first
-      
+
       if options.nil?
         options = @default_grids[name.to_sym]
       elsif options.is_a?(String)
         values = options.split('x')
-        
+
         options = {}
         options.merge!(:width   => values.first.to_i) if values.first
-        options.merge!(:height  => values.last.to_i)  if values.last
+        options.merge!(:height  => values.last.to_i)  if values.second
 
         options = @default_grids[name.to_sym].merge(options)
       elsif options.is_a?(Hash)
         options = @default_grids[name.to_sym].merge(options)
       end
-      
+
       @styles.merge!(name.to_sym => @default_options.merge(options))
+    end
+
+    def styles
+      @styles.empty? ? nil : @styles
     end
   end
 end
