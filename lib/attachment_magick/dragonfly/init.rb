e04e8ffe23398a0f053b1a7f926daa6d84d916a9
Marco Singer <markaum@gmail.com>
2011-07-29 15:48:10 -0300

    bugfix - when the mongoDB needs auth, the gem can't insert files into the DB

--- lib/attachment_magick/dragonfly/init.rb
+++ lib/attachment_magick/dragonfly/init.rb
@@ -13,8 +13,11 @@ if defined? Mongoid::Document
   require 'mongoid'
 
   mongo_yml_path  = Rails.env.test? ? "#{File.expand_path('../../../../test/dummy/config', __FILE__)}" : "config"
-  db              = YAML.load_file(File.join(mongo_yml_path, 'mongoid.yml'))[Rails.env]['database']
-  app.datastore   = Dragonfly::DataStorage::MongoDataStore.new :database => db
+  yaml_file       = YAML.load_file(File.join(mongo_yml_path, 'mongoid.yml'))[Rails.env]
+  db              = yaml_file['database']
+  username        = yaml_file['username']
+  password        = yaml_file['password']
+  app.datastore   = Dragonfly::DataStorage::MongoDataStore.new(:database => db, :username => username, :password => password)
 
   app.define_macro_on_include(Mongoid::Document, :image_accessor)
 end
