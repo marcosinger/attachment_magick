48ec8108f69a3e2e65e5e195f3531952596bacd8
Lucas Renan <lucas@freelancersbrasil.com>
2011-10-27 11:46:00 -0200

    mongoid railtie bug fix

--- app/models/attachment_magick/active_record_image.rb
+++ app/models/attachment_magick/active_record_image.rb
@@ -1,26 +1,33 @@
 module AttachmentMagick
-  class ActiveRecordImage < ActiveRecord::Base
-    set_table_name "amagick_images"
+  if AttachmentMagick.configuration.orms.include?("ActiveRecord")
+    class ActiveRecordImage < ActiveRecord::Base
+      set_table_name "amagick_images"
 
-    belongs_to      :imageable, :polymorphic => true
-    image_accessor  :photo
+      belongs_to      :imageable, :polymorphic => true
+      image_accessor  :photo
 
-    attr_accessor :file_name #not implemented yet
+      attr_accessor :file_name #not implemented yet
 
-    auto_html_for :source => "_to_html" do
-      youtube(:width => 620, :height => 465)
-      vimeo(:width => 620, :height => 465)
+      auto_html_for :source => "_to_html" do
+        youtube(:width => 620, :height => 465)
+        vimeo(:width => 620, :height => 465)
+      end
+
+      auto_html_for :source => "_to_image" do
+        youtube_image
+        vimeo_image(:size => :large)
+      end
+
+      #not implemented yet
+      def is_flash?
+        false
+      end
     end
-
-    auto_html_for :source => "_to_image" do
-      youtube_image
-      vimeo_image(:size => :large)
+  else
+    #mongoid hack
+    #mongoid v2.3 tries to read all the models =/  
+    class ActiveRecordImage
     end
-
-    #not implemented yet
-    def is_flash?
-      false
-    end
-
-  end if AttachmentMagick.configuration.orms.include?("ActiveRecord")
+  end
+  
 end
