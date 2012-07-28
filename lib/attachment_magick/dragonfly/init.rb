b77b30c267302f7b1d01454b168d2cfcf3d6cb30
Marco Singer <markaum@gmail.com>
2012-07-28 14:24:56 -0300

    using Dragonfly :db option to configure access to MongoDB

--- lib/attachment_magick/dragonfly/init.rb
+++ lib/attachment_magick/dragonfly/init.rb
@@ -10,21 +10,6 @@ if defined? ActiveRecord::Base
 end
 
 if defined? Mongoid::Document
-  require 'mongoid'
-
-  mongo_yml_path  = Rails.env.test? ? "#{File.expand_path('../../../../test/dummy/config', __FILE__)}" : "config"
-  yaml_file       = YAML.load_file(File.join(mongo_yml_path, 'mongoid.yml'))[Rails.env]
-  path            = File.join(mongo_yml_path, 'mongoid.yml')
-  yaml_file       = YAML.load(ERB.new(File.new(path).read).result)[Rails.env]
-
-  app.datastore = Dragonfly::DataStorage::MongoDataStore.new
-  app.datastore.configure do |c|
-    c.host      = yaml_file['host'] || yaml_file['uri'] # defaults to localhost
-    c.port      = yaml_file['port']                     # defaults to mongo default (27017)
-    c.database  = yaml_file['database']                 # defaults to 'dragonfly'
-    c.username  = yaml_file['username']                 # only needed if mongo is running in auth mode
-    c.password  = yaml_file['password']                 # only needed if mongo is running in auth mode
-  end
-
+  app.datastore = Dragonfly::DataStorage::MongoDataStore.new :db => Mongoid.database
   app.define_macro_on_include(Mongoid::Document, :image_accessor)
 end
